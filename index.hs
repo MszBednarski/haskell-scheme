a3f4efd4086dbf44ad676d9b79a3149793226941
MszBednarski <msz.bednarski@gmail.com>
2021-04-30 16:17:09 +0200

    week7.

--- index.hs
+++ index.hs
@@ -1,34 +1,44 @@
 main :: IO ()
-double, quad :: Num a => a -> a
+-- up to week 6
+-- double, quad :: Num a => a -> a
 -- :cmd return (unlines [":r", "main"])
 -- haskell is left associating!
 -- factorial x = if x == 1 || x == 0 then 1 else factorial (x-1)*x
-quad x = x^4
-double x = 2*x
-min a b = if a < b then a else b
-max a b = if a > b then a else b
-fib x
-    | x > 1 = fib (x-1) + fib(x-2)
-    | x <= 1 = 1
+-- quad x = x^4
+-- double x = 2*x
+-- min a b = if a < b then a else b
+-- max a b = if a > b then a else b
+-- fib x
+--     | x > 1 = fib (x-1) + fib(x-2)
+--     | x <= 1 = 1
 
-induction base comb n 
-    | n==0 = base
-    | n>0 = comb n (induction base comb (n-1))
+-- induction base comb n 
+--     | n==0 = base
+--     | n>0 = comb n (induction base comb (n-1))
 
-factorial n = (induction 1 (*) n)
+-- factorial n = (induction 1 (*) n)
+
+-- week 7
+-- apply :: (a -> b) -> a -> b
+-- apply f x = f x
+-- add7 x = "Asef"
+
+-- roots (a,b,c)
+--     | a == 0 = error "not quadratic equation"
+--     | (d < 0) = error "complex roots"
+--     | otherwise = ((-b-r))
+--     where {
+--     d = b*b - 4*a*c;
+--     r = sqrt d;
+--     e = 2*a }
+
+-- cartProd :: [a] -> [b] -> [(a,b)]
+-- cartProd xs ys = [(x,y) | x <- xs, y <- ys]
+
+
 
 main = do
     let ps x = putStrLn x
     let pi x = putStrLn $ show x
-    pi $ quad 5.0
-    pi $ double 5
-    pi $ double 5.5
-    pi $ Main.max 5 4
-    pi $ Main.min 4 5
-    pi $ fib 2
-    pi $ fib 3
-    pi $ fib 4
-    pi $ fib 5
-    pi $ fib 6
-    let res = factorial 4
+
     return ()
