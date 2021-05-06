97f46aed2480198dad417716294d8b451f483515
MszBednarski <msz.bednarski@gmail.com>
2021-05-06 11:48:44 +0200

    init parser.

--- parser.hs
+++ parser.hs
@@ -22,6 +22,7 @@ parseString = do
                 x <- many (noneOf "\"")
                 char '"'
                 return $ String x
+ 
 
 parseAtom :: Parser LispVal
 parseAtom = do
@@ -41,15 +42,36 @@ parseNumber :: Parser LispVal
 --                return $ Number n
 parseNumber = many1 digit >>= \r -> return $ Number $ read r
 
+
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
 		 <|> parseString
 		 <|> parseNumber
+         <|> parseQuoted
+         <|> do char '('
+                x <- try parseList <|> parseDottedList
+                char ')'
+                return x
+
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+    head <- endBy parseExpr spaces
+    tail <- char '.' >> spaces >> parseExpr
+    return $ DottedList head tail
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+        char '\''
+        x <- parseExpr
+        return $ List [Atom "quote", x]
 
 readExpr :: String -> String
 readExpr input = case parse parseExpr "lisp" input of
 	Left err -> "No match: " ++ show err
-	Right val -> "Found value"
+	Right val -> "Found val."
 
 
 main :: IO ()
